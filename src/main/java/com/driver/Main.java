f4884743918b049ae8460c0f11bd658f9eb72d95
Saubhagya <saubhagyagupta8@gmail.com>
2023-04-12 16:26:01 +0530

    task completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        B obj = new B();
+        String before = obj.meth();
+        System.out.println(before);
+        String after = obj.meth();
+        System.out.println(after);
+    }
+}
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A{
+    @Override
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
 }
